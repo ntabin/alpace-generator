53b7a9d22b7a1a015d06a7152c760d902e0c797b
ntabin <natsutabs@gmail.com>
2024-10-19 10:32:25 +0800

    redesign

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ document.querySelectorAll('#categoryContainer input[type="radio"]').forEach(elem
         const selected = e.target.value;
         document.querySelectorAll(`#optionsContainer input[type="radio"]`).forEach(option => {
             if(option.getAttribute('name') === selected) {
-                option.closest('.radio-container').classList.remove('d-none');
+                option.parentElement.classList.remove('d-none');
             }
             else {
-                option.closest('.radio-container').classList.add('d-none');
+                option.parentElement.classList.add('d-none');
             }
         });
     });
@@ -43,57 +43,3 @@ document.getElementById('btnRandom').addEventListener('click', e => {
         randomOptionElement.dispatchEvent(new Event('change'))
     });
 });
-
-document.getElementById('btnSave').addEventListener('click', async e => {
-    const canvas = await html2canvas(document.querySelector('.image-container'));
-    console.log('canvas', canvas);
-    //saveAs(canvas.toDataURL(), 'file-name.png');
-    document.body.appendChild(canvas);
-
-    // const c = document.querySelector('canvas');
-    // const anchor = document.createElement("a");
-    // anchor.href = c.toDataURL("image/png");
-    // anchor.download = "IMAGE.PNG";
-    // anchor.click();
-
-
-    // let node = document.querySelector('.image-container');
-    // new Promise((resolve, reject) => {
-    //     node.style.borderRadius = "0px";
-    // }).then(html2canvas(node, {scale: 2}).then(function (canvas) {
-    //     document.body.appendChild(canvas);
-    //     let a = document.createElement("a");
-    //     a.href = canvas.toDataURL();
-    //     a.download = "alpaca";
-    //     a.id = "download-link";
-    //     document.body.appendChild(a);
-    //     a.click();
-    //     node.style.borderRadius = "16px";
-    // }));
-
-});
-
-function saveAs(uri, filename) {
-
-    var link = document.createElement('a');
-
-    if (typeof link.download === 'string') {
-
-        link.href = uri;
-        link.download = filename;
-
-        //Firefox requires the link to be in the body
-        document.body.appendChild(link);
-
-        //simulate click
-        link.click();
-
-        //remove the link when done
-        document.body.removeChild(link);
-
-    } else {
-
-        window.open(uri);
-
-    }
-}
